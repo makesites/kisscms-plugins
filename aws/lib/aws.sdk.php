c940f6714c583f783e820fee1a002413c7f4e6e6
Makis Tracend <makis.tracend@gmail.com>
2012-02-01 14:31:15 -0800

    Adding optional flag for localhost to cover development locally

--- aws/lib/aws.sdk.php
+++ aws/lib/aws.sdk.php
@@ -1,6 +1,11 @@
 <?php 
 
 // Location of the AWS SDK
-include_once( realpath("../../") ."/aws_sdk/sdk.class.php");
+// - optionally define a different location for localhost and production
+if(IS_LOCALHOST){
+	include_once( realpath("../../") ."/aws_sdk/sdk.class.php");
+} else {
+	include_once( realpath("../../") ."/aws_sdk/sdk.class.php");	
+}
 
 ?>
